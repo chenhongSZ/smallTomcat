20aa4a5e39898358e8fd2b69255e598c5e977ecf
atomchen <atomchen@fenqile.com>
2016-10-08 09:28:47 +0800

    chore: 删除logback配置文件 目前不需要

---
+++ src/main/java/com/server/StaticResourceProcessor.java
@@ -0,0 +1,17 @@
+package com.server;
+
+import com.base.Request;
+import com.base.Response;
+
+import java.io.IOException;
+
+public class StaticResourceProcessor {
+    public void process(Request request, Response response) {
+        try {
+            response.sendStaticResource();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
